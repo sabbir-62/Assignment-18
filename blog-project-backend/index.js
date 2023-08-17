95f3f650dd87a39f00754936f6ede70979d3088c
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-08-17 15:48:51 +0600

    first

--- blog-project-backend/index.js
+++ blog-project-backend/index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
+require("dotenv").config();
+const port = process.env.port || 8000
 
-app.listen(process.env.port, () => {
-    console.log("Server is running port on  8000");
+app.listen(port, () => {
+    console.log(`Server is running port on ${port}`  );
 })
